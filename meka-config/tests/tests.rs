ae49c7ab5eb5a99dd1a9bca83034050e2bcc9052
Andy Weidenbaum <atweiden@ioiojo.com>
2025-06-21 06:34:43 +0000

    meka-config: fix tests, mostly

--- meka-config/tests/tests.rs
+++ meka-config/tests/tests.rs
@@ -3,10 +3,9 @@ fn it_works() {
     use meka_config::{Config, Env};
     use mlua_module_manifest::{Module, ModuleFileType, ModuleNamedText};
     use std::borrow::Cow;
-    use std::collections::HashMap;
     use std::convert::From;
 
-    let module = r#"(local fennel-src (require :fennel-src))
+    let module: &str = r#"(local fennel-src (require :fennel-src))
 (local meka (require :meka))
 (local manifest meka.manifest)
 
@@ -34,14 +33,14 @@ fn it_works() {
     );
     let env: Option<Env> = None;
     // `fennel-src` should be available by default to `mlua::Lua`.
-    assert!(Config::new(module, env).is_ok());
+    assert!(Config::new(module.clone(), env).is_ok());
 
     // Passing equivalent `fennel-src` mapping should make no difference.
     let mut env = Env::new();
     env.insert(Cow::from("fennel-src"), fennel_src::loader);
-    assert!(Config::new(module, Some(env)).is_ok());
+    assert!(Config::new(module, Some(env.clone())).is_ok());
 
-    let module = r#"local fennel_src = require("fennel-src")
+    let module: &str = r#"local fennel_src = require("fennel-src")
 local meka = require("meka")
 local manifest = meka.manifest
 local kiwi = manifest.new({name = "kiwi.cite",            path = "kiwi/cite.fnl"},
@@ -59,37 +58,7 @@ return {kiwi = kiwi}"#;
         Module::NamedText(ModuleNamedText::new("manifest", module, ModuleFileType::Lua).unwrap());
 
     // Passing equivalent Lua config should make no difference.
-    assert!(Config::new(module, None).is_ok());
-    assert!(Config::new(module, env).is_ok());
-}
-
-#[test]
-fn defmanifest_fennel_macro_works() {
-    use meka_config::{Config, Env};
-    use mlua_module_manifest::{Module, ModuleFileType, ModuleNamedText};
-    use std::collections::HashMap;
-
-    let module = r#"(local fennel-src (require :fennel-src))
-
-(defmanifest kiwi {:name :kiwi.cite            :path :kiwi/cite.fnl
-                   :name :kiwi.date            :path :kiwi/date.fnl
-                   :name :kiwi.date-time       :path :kiwi/date-time.fnl
-                   :name :kiwi.dine            :path :kiwi/dine/init.fnl
-                   :name :kiwi.dine.proximates :path :kiwi/dine/proximates.fnl
-                   :name :kiwi.food            :path :kiwi/food/init.fnl
-                   :name :kiwi.food.proximates :path :kiwi/food/proximates.fnl
-                   :name :kiwi.time            :path :kiwi/time.fnl
-                   :name :kiwi.utils           :path :kiwi/utils.fnl}
-                   (fennel-src))"#
-        .into();
-    let module = Module::NamedText(
-        ModuleNamedText::new("manifest", module, ModuleFileType::Fennel).unwrap(),
-    );
-    let env: Option<Env> = None;
-    assert!(Config::new(module, env).is_ok());
-
-    let mut env = Env::new();
-    env.insert(Cow::from("fennel-src"), fennel_src::loader);
+    assert!(Config::new(module.clone(), None).is_ok());
     assert!(Config::new(module, Some(env)).is_ok());
 }
 
@@ -97,9 +66,48 @@ fn defmanifest_fennel_macro_works() {
 fn manifest_fennel_macro_works() {
     use meka_config::{Config, Env};
     use mlua_module_manifest::{Module, ModuleFileType, ModuleNamedText};
-    use std::collections::HashMap;
+    use std::borrow::Cow;
+    use std::convert::From;
 
-    let module = r#"(local fennel-src (require :fennel-src))
+    let module: &str = r#"(local fennel-src (require :fennel-src))
+
+;; Meka automatically imports the manifest macro. The following is short for:
+;;
+;;     (local kiwi ((. (require :meka) :manifest :new) {...}))
+;;
+(local kiwi (manifest {:name :kiwi.cite            :path :kiwi/cite.fnl}
+                      {:name :kiwi.date            :path :kiwi/date.fnl}
+                      {:name :kiwi.date-time       :path :kiwi/date-time.fnl}
+                      {:name :kiwi.dine            :path :kiwi/dine/init.fnl}
+                      {:name :kiwi.dine.proximates :path :kiwi/dine/proximates.fnl}
+                      {:name :kiwi.food            :path :kiwi/food/init.fnl}
+                      {:name :kiwi.food.proximates :path :kiwi/food/proximates.fnl}
+                      {:name :kiwi.time            :path :kiwi/time.fnl}
+                      {:name :kiwi.utils           :path :kiwi/utils.fnl}
+                      ;; Embed Fennel via `fennel-src` crate. Enables `(require :fennel)`.
+                      (fennel-src)))
+
+{: kiwi}"#
+        .into();
+    let module = Module::NamedText(
+        ModuleNamedText::new("manifest", module, ModuleFileType::Fennel).unwrap(),
+    );
+    let env: Option<Env> = None;
+    assert!(Config::new(module.clone(), env).is_ok());
+
+    let mut env = Env::new();
+    env.insert(Cow::from("fennel-src"), fennel_src::loader);
+    assert!(Config::new(module, Some(env)).is_ok());
+}
+
+#[test]
+fn standalone_manifest_fennel_macro_works() {
+    use meka_config::{Config, Env};
+    use mlua_module_manifest::{Module, ModuleFileType, ModuleNamedText};
+    use std::borrow::Cow;
+    use std::convert::From;
+
+    let module: &str = r#"(local fennel-src (require :fennel-src))
 
 ;; Refer to this manifest in `meka_include!` or `meka_load!` by omitting a string argument.
 (manifest {:name :kiwi.cite            :path :kiwi/cite.fnl
@@ -117,9 +125,9 @@ fn manifest_fennel_macro_works() {
         ModuleNamedText::new("manifest", module, ModuleFileType::Fennel).unwrap(),
     );
     let env: Option<Env> = None;
-    assert!(Config::new(module, env).is_ok());
+    assert!(Config::new(module.clone(), env).is_ok());
 
     let mut env = Env::new();
     env.insert(Cow::from("fennel-src"), fennel_src::loader);
-    assert!(Config::new(arg, Some(env)).is_ok());
+    assert!(Config::new(module, Some(env)).is_ok());
 }
