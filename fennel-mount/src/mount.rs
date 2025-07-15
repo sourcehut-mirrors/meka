044e9d0b468159ebe2489306254524f534930f79
Andy Weidenbaum <atweiden@ioiojo.com>
2025-07-15 13:34:58 +0000

    fennel-mount: s/unload/unload_module

--- fennel-mount/src/mount.rs
+++ fennel-mount/src/mount.rs
@@ -28,7 +28,7 @@ impl Mount for Lua {
             (true, _) => {
                 // Remove `fennel` module from `package.loaded` cache unless it was there already.
                 if !package_loaded_contains_fennel {
-                    self.unload("fennel")?;
+                    self.unload_module("fennel")?;
                 }
             }
             (false, _) => {
