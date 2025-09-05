d9e5d1de9a628d6e8550a8ff0f24d9b7bc297fc6
Andy Weidenbaum <atweiden@ioiojo.com>
2025-09-05 13:30:33 +0000

    fennel-utils: use package_searchers_or_loaders

--- fennel-utils/src/lib.rs
+++ fennel-utils/src/lib.rs
@@ -4,10 +4,6 @@ pub mod prelude {
     pub use crate::{FennelView, InsertFennelSearcher};
 }
 
-/// Error message designed for running `table.get(key)` on `mlua::Table` `table` verified to
-/// contain key `key`.
-const TABLE_GET_EXPECT: &str = "Unexpectedly couldn't get key from pre-checked table";
-
 pub trait FennelView {
     fn fennel_view(&self, value: Value, opts: Option<Table>) -> mlua::Result<String>;
 }
@@ -61,45 +57,24 @@ impl InsertFennelSearcher for Lua {
             )
         })?;
 
-        let package: Table = self.globals().get("package").map_err(|_| {
-            mlua::Error::RuntimeError(
-                "fennel-utils insert_fennel_searcher function couldn't get Lua package table"
-                    .to_string(),
-            )
+        let package_searchers: Table = mlua_utils::package_searchers_or_loaders(self).map_err(|e| {
+            mlua::Error::RuntimeError(format!("fennel-utils insert_fennel_searcher function couldn't get Lua package.searchers or package.loaders table: {}", e))
         })?;
 
-        let package_loaders: Table = if package.contains_key("loaders").map_err(|_| {
-            mlua::Error::RuntimeError(
-                "fennel-utils insert_fennel_searcher function couldn't check if package table contains loaders key"
-                    .to_string(),
-            )
-        })? {
-            package.get("loaders").expect(TABLE_GET_EXPECT)
-        } else if package.contains_key("searchers").map_err(|_| {
-            mlua::Error::RuntimeError(
-                "fennel-utils insert_fennel_searcher function couldn't check if package table contains searchers key"
-                    .to_string(),
-            )
-        })? {
-            package.get("searchers").expect(TABLE_GET_EXPECT)
-        } else {
-            return Err(mlua::Error::RuntimeError("fennel-utils insert_fennel_searcher function couldn't find either Lua package.loaders or package.searchers table".to_string()));
-        };
-
-        let package_loaders_len = package_loaders.len().map_err(|_| {
+        let package_searchers_len = package_searchers.len().map_err(|_| {
             mlua::Error::RuntimeError("fennel-utils insert_fennel_searcher function couldn't get length of Lua package.loaders (or package.searchers) table".to_string())
         })?;
 
         // There are 4 seachers in `package.searchers` by default (see: `loadlib.c` in Lua
         // source code), but just in case:
-        if package_loaders_len > 2 {
-            package_loaders
-                .raw_insert(package_loaders_len - 2, fennel_searcher)
+        if package_searchers_len > 2 {
+            package_searchers
+                .raw_insert(package_searchers_len - 2, fennel_searcher)
                 .map_err(|_| {
                     mlua::Error::RuntimeError("fennel-utils insert_fennel_searcher function couldn't insert Fennel searcher in package.loaders (or package.searchers) table at index before last two searchers".to_string())
                 })?;
         } else {
-            package_loaders
+            package_searchers
                 .push(fennel_searcher)
                 .map_err(|_| {
                     mlua::Error::RuntimeError("fennel-utils insert_fennel_searcher function couldn't append Fennel searcher to package.loaders (or package.searchers) table".to_string())
